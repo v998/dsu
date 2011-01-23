90980f81a5d093375e4759ae1ef083e996ea6090
kookxiang@foxmail.com <kookxiang@foxmail.com@f1529acb-6d0f-4040-ab86-74da46a47b73>
2011-01-23 16:00:54 +0000

    增加多语言 增加自动更新支持

--- trunk/PluginUpdater/dsu_updater/updater.inc.php
+++ trunk/PluginUpdater/dsu_updater/updater.inc.php
@@ -5,7 +5,7 @@ if(!defined('IN_DISCUZ') || !defined('IN_ADMINCP')) exit('Access Denied');
 $plugin_id='dsu_updater';
 @include DISCUZ_ROOT."./data/plugindata/{$plugin_id}.lang.php";
 $xml_name=$scriptlang[$plugin_id]['xml_file_name'];
-$program_ver='0.1 Beta';
+$program_ver='0.2 Beta';
 $plugin_dir=DISCUZ_ROOT."./source/plugin/{$plugin_id}";
 $plugin_subfolders=array('template');
 $plugin_files=array(
