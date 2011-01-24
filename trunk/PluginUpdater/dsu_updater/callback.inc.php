4e1d03fc29ee4691107770522c4390efbe63640a
kookxiang@foxmail.com <kookxiang@foxmail.com@f1529acb-6d0f-4040-ab86-74da46a47b73>
2011-01-24 05:56:16 +0000

    修复bug 对授权弹窗的小调整

--- trunk/PluginUpdater/dsu_updater/callback.inc.php
+++ trunk/PluginUpdater/dsu_updater/callback.inc.php
@@ -16,7 +16,7 @@ switch($_G['gp_do']){
 			$_G['dsu_updater']['site_id']=$site_id;
 			$_G['dsu_updater']['key']=$key;
 			save_setting();
-			showmessage($du_lang['accept_succeed'],'admin.php?frames=yes&action=plugins&operation=config&identifier=dsu_updater&pmod=news');
+			exit("<a href=\"\" onclick=\"window.close();\" onload=\"window.close();\">{$du_lang[accept_succeed]}</a>");
 		}else{
 			include template('dsu_updater:oauth');
 		}
@@ -36,14 +36,15 @@ switch($_G['gp_do']){
 		$site_id=$_G['gp_site_id']?intval($_G['gp_site_id']):'';
 		$key=strip_tags($_G['gp_key']);
 		if(!check_key($site_id,$key)) exit('E0');
-		$file_list=unserialize($_GET['file_list']);
+		$file_list=unserialize($_POST['file_list']);
 		$files=array();
 		foreach($file_list as $path=>$contents){
 			if(!is_writeable(DISCUZ_ROOT.'./'.$path)) exit('E1');
 			unlink(DISCUZ_ROOT.'./'.$path);
 			file_put_contents(DISCUZ_ROOT.'./'.$path,$contents);
 		}
-		exit('ok');
+		$pluginid=DB::result_first('SELECT pluginid FROM '.DB::table('common_plugin')." WHERE identifier='{$_G[gp_plugin]}'");
+		exit("ok|$pluginid");
 		break;
 	case 'check':
 		get_setting();
