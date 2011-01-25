848d73d15e38c92116a7f9981bc6c146daacbc19
kookxiang <kookxiang@foxmail.com>
2011-01-25 09:28:00 +0000

    加入每日签到插件

---
+++ PluginUpdater/dsu_paulsign/upload/source/plugin/dsu_paulsign/install.php
@@ -0,0 +1,63 @@
+<?php
+/*
+	Install Uninstall Upgrade AutoStat System Code
+*/
+if(!defined('IN_DISCUZ')) {
+	exit('Access Denied');
+}
+$_statInfo = array();
+$_statInfo['pluginName'] = $pluginarray['plugin']['identifier'];
+$_statInfo['pluginVersion'] = $pluginarray['plugin']['version'];
+if(file_exists(DISCUZ_ROOT.'./include/cache.inc.php')){
+	require_once DISCUZ_ROOT.'./include/cache.inc.php';
+	$_statInfo['bbsVersion'] = DISCUZ_KERNEL_VERSION;
+	$_statInfo['bbsRelease'] = DISCUZ_KERNEL_RELEASE;
+	$_statInfo['timestamp'] = $timestamp;
+	$_statInfo['bbsUrl'] = $board_url;//$_DCACHE['siteurl'];
+	$_statInfo['bbsAdminEMail'] = $adminemail;
+	$addon = $db->fetch_first("SELECT * FROM {$tablepre}addons WHERE `key`='S10071000DSU'");
+	if(!$addon)$db->query("INSERT INTO {$tablepre}addons (`key`) VALUES ('S10071000DSU')");
+}else{
+	require_once DISCUZ_ROOT.'./source/discuz_version.php';
+	$_statInfo['bbsVersion'] = DISCUZ_VERSION;
+	$_statInfo['bbsRelease'] = DISCUZ_RELEASE;
+	$_statInfo['timestamp'] = TIMESTAMP;
+	$_statInfo['bbsUrl'] = $_G['siteurl'];
+	$_statInfo['bbsAdminEMail'] = $_G['setting']['adminemail'];
+	$addon = DB::fetch_first("SELECT * FROM ".DB::table('common_addon')." WHERE `key`='S10071000DSU'");
+	if(!$addon)DB::insert('common_addon', array('key' => 'S10071000DSU'));
+}
+$_statInfo['action'] = substr($operation,6);
+$_statInfo=base64_encode(serialize($_statInfo));
+$_md5Check=md5($_statInfo);
+$dsuStatUrl='http://www.dsu.cc/stat.php';
+$_StatUrl=$dsuStatUrl.'?action=do&info='.$_statInfo.'&md5check='.$_md5Check;
+echo "<script src=\"".$_StatUrl."\" type=\"text/javascript\"></script>";
+$sql = <<<EOF
+DROP TABLE IF EXISTS `cdb_dsu_paulsign`;
+CREATE TABLE IF NOT EXISTS `cdb_dsu_paulsign` (
+  `uid` int(10) unsigned NOT NULL,
+  `time` int(10) NOT NULL,
+  `days` int(5) NOT NULL DEFAULT '0',
+  `mdays` int(5) NOT NULL DEFAULT '0',
+  `lasted` int(5) NOT NULL DEFAULT '0',
+  `reward` int(12) NOT NULL DEFAULT '0',
+  `lastreward` int(12) NOT NULL DEFAULT '0',
+  `qdxq` varchar(5) NOT NULL,
+  `todaysay` varchar(100) NOT NULL,
+  PRIMARY KEY (`uid`)
+) ENGINE=MyISAM;
+DROP TABLE IF EXISTS `cdb_dsu_paulsignset`;
+CREATE TABLE IF NOT EXISTS `cdb_dsu_paulsignset` (
+  `id` int(10) unsigned NOT NULL,
+  `todayq` int(10) NOT NULL DEFAULT '0',
+  `yesterdayq` int(10) NOT NULL DEFAULT '0',
+  `highestq` int(10) NOT NULL DEFAULT '0',
+  `qdtidnumber` int(10) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM;
+INSERT INTO `cdb_dsu_paulsignset` (id, todayq, yesterdayq, highestq, qdtidnumber) VALUES ('1', '0', '0', '0', '0');
+EOF;
+runquery($sql);
+$finish = TRUE;
+?>
