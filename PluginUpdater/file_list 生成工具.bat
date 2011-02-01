9519601968451f610124e0e3e8c62855e6b49341
kookxiang <kookxiang@foxmail.com>
2011-02-01 08:56:33 +0000

    File List 生成工具

--- PluginUpdater/file_list 生成工具.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-TITLE file_list 生成工具
-set /p plugin_id=请输入插件id:
-cd %plugin_id%/upload
-xcopy . .. /l /e > ../file_list
-cls
-echo 生成完毕，请手工将file_list中的斜杠替换，并对尾部进行处理！
-echo 按任意键退出
-pause > nul
