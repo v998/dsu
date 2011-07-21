ce4b205b77fd70c7a5f091c0ab7b7581c6c4b67c
marco.website01@gmail.com <marco.website01@gmail.com@f1529acb-6d0f-4040-ab86-74da46a47b73>
2011-07-21 11:42:43 +0000

    我是神偷 dsu_marcothief V1.3

--- trunk/PluginUpdater/dsu_marcothief/upload/source/plugin/dsu_marcothief/dsu_marcothief.func.php
+++ trunk/PluginUpdater/dsu_marcothief/upload/source/plugin/dsu_marcothief/dsu_marcothief.func.php
@@ -8,12 +8,24 @@ if(!defined('IN_DISCUZ')) {
 	exit('Access Denied');
 }
 
+function is_admin(){
+	global $_G, $config;
+	$admin_array = explode(',', $config['admin_list']);
+	if(!in_array($_G['uid'], $admin_array)){
+		showmessage('group_nopermission', 'plugin.php?id=dsu_marcothief', array('grouptitle' => $_G['group']['grouptitle']));
+	}
+}
+
 function log_add($username, $type, $extra = array()){
 	global $_G, $config;
 	if($type == 'thief_sucess'){
 		$action = lang('plugin/dsu_marcothief', 'log_thief_sucess', array('username' => $username, 'earn' => $_G['setting']['extcredits'][$config['credit']]['title'].$extra['thief_get'].$_G['setting']['extcredits'][$config['credit']]['unit']));
 	}elseif($type == 'thief_fail'){
 		$action = lang('plugin/dsu_marcothief', 'log_thief_fail', array('username' => $username));
+	}elseif($type == 'log_fail_guard'){
+		$action = lang('plugin/dsu_marcothief', 'log_fail_guard', array('username' => $username));
+	}elseif($type == 'thief_fail_police'){
+		$action = lang('plugin/dsu_marcothief', 'log_fail_police', array('username' => $username));
 	}elseif($type == 'thief_fail_jail'){
 		$action = lang('plugin/dsu_marcothief', 'log_fail_jail', array('username' => $username));
 	}elseif($type == 'jail_raids'){
@@ -24,6 +36,21 @@ function log_add($username, $type, $extra = array()){
 	if($action){
 		DB::query("INSERT INTO ".DB::table('dsu_marcothief_log')." (log,time) VALUES ('$action','$_G[timestamp]')");
 		return TRUE;
+	}else{
+		return FALSE;
+	}
+}
+
+function use_shop($uid, $type){
+	global $_G, $config;
+	if(in_array($type, array('1', '2', '3'))){
+		$type_detail = array(1 => 'weapon', 2 => 'raids_tool', 3 => 'run_tool');
+		$shop_db = DB::fetch_first("SELECT * FROM ".DB::table('dsu_marcothief')." t,".DB::table('dsu_marcothief_shop')." s WHERE t.{$type_detail[$type]}=s.id AND t.uid='$_G[uid]'");
+		if(!$shop_db){
+			return FALSE;
+		}else{
+			return array($shop_db['function']/100, $shop_db['function']);
+		}
 	}else{
 		return FALSE;
 	}
--- trunk/PluginUpdater/dsu_marcothief/upload/source/plugin/dsu_marcothief/upgrade.php
+++ trunk/PluginUpdater/dsu_marcothief/upload/source/plugin/dsu_marcothief/upgrade.php
@@ -32,6 +32,15 @@ $dsuStatUrl='http://www.dsu.cc/stat.php';
 $_StatUrl=$dsuStatUrl.'?action=do&info='.$_statInfo.'&md5check='.$_md5Check;
 echo "<script src=\"".$_StatUrl."\" type=\"text/javascript\"></script>";
 
+$dsu_marcothief_bag = <<<EOF
+CREATE TABLE IF NOT EXISTS `pre_dsu_marcothief_bag` (
+  `id` int(8) unsigned NOT NULL AUTO_INCREMENT,
+  `uid` int(8) unsigned NOT NULL,
+  `shopid` int(8) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM;
+EOF;
+
 $dsu_marcothief_log = <<<EOF
 CREATE TABLE IF NOT EXISTS `pre_dsu_marcothief_log` (
   `id` int(8) unsigned NOT NULL AUTO_INCREMENT,
@@ -41,26 +50,78 @@ CREATE TABLE IF NOT EXISTS `pre_dsu_marcothief_log` (
 ) ENGINE=MyISAM;
 EOF;
 
+$dsu_marcothief_shop = <<<EOF
+CREATE TABLE IF NOT EXISTS `pre_dsu_marcothief_shop` (
+  `id` int(8) unsigned NOT NULL AUTO_INCREMENT,
+  `type` int(8) unsigned NOT NULL DEFAULT '0',
+  `name` char(80) NOT NULL,
+  `intro` text NOT NULL,
+  `function` text NOT NULL,
+  `price` int(8) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM;
+EOF;
+
+$dsu_marcothief_shop_data = <<<EOF
+INSERT INTO `pre_dsu_marcothief_shop` (`id`, `type`, `name`, `intro`, `function`, `price`) VALUES
+(1, 1, '$installlang[shop_1]', '$installlang[shop_2]', '5', 100),
+(2, 1, '$installlang[shop_3]', '$installlang[shop_4]', '10', 250),
+(3, 2, '$installlang[shop_5]', '$installlang[shop_6]', '5', 100),
+(4, 2, '$installlang[shop_7]', '$installlang[shop_8]', '10', 250),
+(5, 3, '$installlang[shop_9]', '$installlang[shop_10]', '5', 100),
+(6, 3, '$installlang[shop_11]', '$installlang[shop_12]', '10', 250);
+EOF;
+
 $sql1 = <<<EOF
-ALTER TABLE `pre_dsu_marcothief` ADD `raids` INT( 8 ) NOT NULL DEFAULT '0' AFTER `actions`
-ALTER TABLE `pre_dsu_marcothief` ADD `run` INT( 10 ) NOT NULL AFTER `jail`
+ALTER TABLE `pre_dsu_marcothief` ADD `raids` INT( 8 ) NOT NULL DEFAULT '0' AFTER `actions`;
+ALTER TABLE `pre_dsu_marcothief` ADD `run` INT( 10 ) NOT NULL AFTER `jail`;
 EOF;
 
 $sql2 = <<<EOF
-ALTER TABLE `pre_dsu_marcothief` ADD `goodluck` INT( 10 ) NOT NULL AFTER `run`
+ALTER TABLE `pre_dsu_marcothief` ADD `goodluck` INT( 10 ) NOT NULL AFTER `run`;
 EOF;
 
+$sql3 = <<<EOF
+ALTER TABLE `pre_dsu_marcothief` ADD `weapon` INT( 8 ) UNSIGNED NOT NULL DEFAULT '0' AFTER `goodluck`;
+ALTER TABLE `pre_dsu_marcothief` ADD `raids_tool` INT( 8 ) UNSIGNED NOT NULL DEFAULT '0' AFTER `weapon`;
+ALTER TABLE `pre_dsu_marcothief` ADD `run_tool` INT( 8 ) UNSIGNED NOT NULL DEFAULT '0' AFTER `raids_tool`;
+ALTER TABLE `pre_dsu_marcothief` ADD `protect` INT( 10 ) NOT NULL AFTER `goodluck`;
+EOF;
+
+$dsu_marcothief_bag_exist = DB::query("SHOW TABLES LIKE '".DB::table('dsu_marcothief_bag')."'");
+$run_bag_convert = (DB::num_rows($dsu_marcothief_bag_exist) <= 0) ? TRUE : FALSE;
+
+runquery($dsu_marcothief_log);
+runquery($dsu_marcothief_shop);
 if($_G['gp_fromversion'] == '1.0'){
 	runquery($sql1);
 	runquery($sql2);
-}elseif($_G['gp_fromversion'] == '1.1'){
+	runquery($sql3);
+	runquery($dsu_marcothief_shop_data);
+}elseif($_G['gp_fromversion'] == '1.1' || $_G['gp_fromversion'] == '1.1.2'){
 	runquery($sql2);
-}elseif($_G['gp_fromversion'] == '1.1.2'){
-	runquery($sql2);
+	runquery($sql3);
+	runquery($dsu_marcothief_shop_data);
+}elseif($_G['gp_fromversion'] == '1.2'){
+	runquery($sql3);
+	runquery($dsu_marcothief_shop_data);
 }
-runquery($dsu_marcothief_log);
 
-@copy(DISCUZ_ROOT.'./source/plugin/dsu_marcothief/dsu_marcothief_daily.php',DISCUZ_ROOT.'./source/include/cron/dsu_marcothief_daily.inc.php');
+if($run_bag_convert == TRUE){
+	$query = DB::query("SELECT uid,weapon,raids_tool,run_tool FROM ".DB::table('dsu_marcothief')."");
+	runquery($dsu_marcothief_bag);
+	while($data = DB::fetch($query)){
+		if($data['weapon'] != 0){
+			DB::query("INSERT INTO ".DB::table('dsu_marcothief_bag')." (uid,shopid) VALUES ('$data[uid]','$data[weapon]')");
+		}
+		if($data['raids_tool'] != 0){
+			DB::query("INSERT INTO ".DB::table('dsu_marcothief_bag')." (uid,shopid) VALUES ('$data[uid]','$data[raids_tool]')");
+		}
+		if($data['run_tool'] != 0){
+			DB::query("INSERT INTO ".DB::table('dsu_marcothief_bag')." (uid,shopid) VALUES ('$data[uid]','$data[run_tool]')");
+		}
+	}
+}
 
 $finish = TRUE;
 ?>
