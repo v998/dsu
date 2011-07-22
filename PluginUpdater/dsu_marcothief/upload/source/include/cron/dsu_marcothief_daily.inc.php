1e3dcda159d20a0a5d9141bcf75ddd613c9e6e0b
marco.website01 <marco.website01@gmail.com>
2011-07-22 18:57:53 +0000

    我是神偷 dsu_marcothief V1.3.1

--- PluginUpdater/dsu_marcothief/upload/source/include/cron/dsu_marcothief_daily.inc.php
+++ PluginUpdater/dsu_marcothief/upload/source/include/cron/dsu_marcothief_daily.inc.php
@@ -7,5 +7,5 @@
 if(!defined('IN_DISCUZ')) {
 	exit('Access Denied');
 }
-DB::query("UPDATE ".DB::table('dsu_marcothief')." SET action='0',actions='0'");
+DB::query("UPDATE ".DB::table('dsu_marcothief')." SET action='0',actions='0'", 'UNBUFFERED');
 ?>
